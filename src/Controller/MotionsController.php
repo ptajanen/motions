6790c2c9e8bcf72127ac3a1f311dc082722d0357
root <root@BC-H8FV7S2.bc.local>
2019-09-29 10:46:32 +0300

    annotations installed and @Route added to the controller

---
+++ src/Controller/MotionsController.php
@@ -0,0 +1,22 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MotionsController
+{
+/**
+ * @Route("/motions/number")
+ */
+    public function number()
+    {
+        $number = random_int(0, 100);
+
+        return new Response(
+            '<html><body>Lucky number: '.$number.'</body></html>'
+        );
+    }
+}
+
+?>
